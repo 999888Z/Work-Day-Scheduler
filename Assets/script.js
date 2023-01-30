f7ba83adfe29e51bc28d8469bdb5faed3cebad13
John <johnwilday@yahoo.com>
2023-01-30 15:58:28 -0600

    Small error fix

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,23 @@
 //added variables to connect local storage to html text area in timeblocks
+var input9AM = $("#input-9AM");
+var input10AM = $("#input-10AM");
+var input11AM = $("#input-11AM");
+var input12PM = $("#input-12PM");
+var input1PM = $("#input-1PM");
+var input2PM = $("#input-2PM");
+var input3PM = $("#input-3PM");
+var input4PM = $("#input-4PM");
+var input5PM = $("#input-5PM");
+//added variable to connect to buttons on each timeblock
+var saveItem = $(".saveBtn");
+//created variables to display current date from day.js on the page
+var currentDay = $("#currentDay");
 
-var input9AM = $('#input-9AM')
-var input10AM = $('#input-10AM')
-var input11AM = $('#input-11AM')
-var input12PM = $('#input-12PM')
-var input1PM = $('#input-1PM')
-var input2PM = $('#input-2PM')
-var input3PM = $('#input-3PM')
-var input4PM = $('#input-4PM')
-var input5PM = $('#input-5PM')
-//created variables to display current date from day.js on page via html
-var currentDay = $('#currentDay');
-
-var now = dayjs().format('dddd, MMMM D')
-  currentDay.text(now)
+var now = dayjs().format("dddd, MMMM D");
+currentDay.text(now);
 //created variable to bring current time from day.js into js
-var timeNow = dayjs().format('hA')
-
-//created empty variable to store ?
+var timeNow = dayjs().format("hA");
+//created an array to iterate through
 var dayTimes = [
   "9AM",
   "10AM",
@@ -27,168 +27,100 @@ var dayTimes = [
   "2PM",
   "3PM",
   "4PM",
-  "5PM"
-]
-//created for loop to iterate through all hours of workday and if 
+  "5PM",
+];
+//created for loop to iterate through all hours of workday
 $(function () {
-   for (let index = 0; index < dayTimes.length; index++) {
-    var eachTime = $(`#${dayTimes[index]}`)
-  eachTime.attr("class", "past row time-block")
+  for (let index = 0; index < dayTimes.length; index++) {
+    var eachTime = $(`#${dayTimes[index]}`);
+    eachTime.attr("class", "past row time-block");
 
-  
-//created if statement to compare current time with all hours of workday
+    //created if statement to compare current time with all hours of workday
     if (timeNow === dayTimes[index]) {
-      eachTime.attr("class", "present row time-block")
+      eachTime.attr("class", "present row time-block");
+    } else if (timeNow === dayTimes[index - 1]) {
+      eachTime.attr("class", "future row time-block");
+    } else if (timeNow === dayTimes[index - 2]) {
+      eachTime.attr("class", "future row time-block");
+    } else if (timeNow === dayTimes[index - 3]) {
+      eachTime.attr("class", "future row time-block");
+    } else if (timeNow === dayTimes[index - 4]) {
+      eachTime.attr("class", "future row time-block");
+    } else if (timeNow === dayTimes[index - 5]) {
+      eachTime.attr("class", "future row time-block");
+    } else if (timeNow === dayTimes[index - 6]) {
+      eachTime.attr("class", "future row time-block");
+    } else if (timeNow === dayTimes[index - 7]) {
+      eachTime.attr("class", "future row time-block");
+    } else if (timeNow === dayTimes[index - 8]) {
+      eachTime.attr("class", "future row time-block");
+    } else if (timeNow === dayTimes[index - 9]) {
+      eachTime.attr("class", "future row time-block");
     }
-//created else if statements to ?
-    else if (timeNow === dayTimes[index-1]) {
-      eachTime.attr("class", "future row time-block")
-}
-
-else if (timeNow === dayTimes[index-2]) {
-  eachTime.attr("class", "future row time-block")
-}
-
-else if (timeNow === dayTimes[index-3]) {
-  eachTime.attr("class", "future row time-block")
-}
-
-else if (timeNow === dayTimes[index-4]) {
-  eachTime.attr("class", "future row time-block")
-}
-
-else if (timeNow === dayTimes[index-5]) {
-  eachTime.attr("class", "future row time-block")
-}
-
-else if (timeNow === dayTimes[index-6]) {
-  eachTime.attr("class", "future row time-block")
-}
-
-else if (timeNow === dayTimes[index-7]) {
-  eachTime.attr("class", "future row time-block")
-}
-
-else if (timeNow === dayTimes[index-8]) {
-  eachTime.attr("class", "future row time-block")
-}
-
-else if (timeNow === dayTimes[index-9]) {
-  eachTime.attr("class", "future row time-block")
-}
-  } 
-  // created function to be triggered by event listener/button click, that will store 
+  }
+  // created function to be triggered by event listener/button click, that will store
   // input text in local storage
-function saveText9AM(event) {
-event.preventDefault();
+  function saveText9AM(event) {
+    event.preventDefault();
 
-if (this.id === "button-9AM") {
-  var  nineAM = input9AM.val();
-  localStorage.setItem('stored9AM', nineAM)
-  
-}
-else if (this.id === "button-10AM") {
-  var  tenAM = input10AM.val();
-  localStorage.setItem('stored10AM', tenAM) 
-}
+    if (this.id === "button-9AM") {
+      var nineAM = input9AM.val();
+      localStorage.setItem("stored9AM", nineAM);
+    } else if (this.id === "button-10AM") {
+      var tenAM = input10AM.val();
+      localStorage.setItem("stored10AM", tenAM);
+    } else if (this.id === "button-11AM") {
+      var elevenAM = input11AM.val();
+      localStorage.setItem("stored11AM", elevenAM);
+    } else if (this.id === "button-12PM") {
+      var twelvePM = input12PM.val();
+      localStorage.setItem("stored12PM", twelvePM);
+    } else if (this.id === "button-1PM") {
+      var onePM = input1PM.val();
+      localStorage.setItem("stored1PM", onePM);
+    } else if (this.id === "button-2PM") {
+      var twoPM = input2PM.val();
+      localStorage.setItem("stored2PM", twoPM);
+    } else if (this.id === "button-3PM") {
+      var threePM = input3PM.val();
+      localStorage.setItem("stored3PM", threePM);
+    } else if (this.id === "button-4PM") {
+      var fourPM = input4PM.val();
+      localStorage.setItem("stored4PM", fourPM);
+    } else if (this.id === "button-5PM") {
+      var fivePM = input5PM.val();
+      localStorage.setItem("stored5PM", fivePM);
+    }
+  }
+  // created methods to retrieve stored input in each timeblock
+  var stored9AM = localStorage.getItem("stored9AM");
+  $("#input-9AM").val(stored9AM);
 
-else if (this.id === "button-11AM") {
-  var  elevenAM = input11AM.val();
-  localStorage.setItem('stored11AM', elevenAM) 
-}
+  var stored10AM = localStorage.getItem("stored10AM");
+  $("#input-10AM").val(stored10AM);
 
-else if (this.id === "button-12PM") {
-  var  twelvePM = input12PM.val();
-  localStorage.setItem('stored12PM', twelvePM) 
-}
+  var stored11AM = localStorage.getItem("stored11AM");
+  $("#input-11AM").val(stored11AM);
 
-else if (this.id === "button-1PM") {
-  var  onePM = input12PM.val();
-  localStorage.setItem('stored1PM', onePM) 
-}
+  var stored12PM = localStorage.getItem("stored12PM");
+  $("#input-12PM").val(stored12PM);
 
-else if (this.id === "button-2PM") {
-  var  twoPM = input2PM.val();
-  localStorage.setItem('stored2PM', twoPM) 
-}
+  var stored1PM = localStorage.getItem("stored1PM");
+  $("#input-1PM").val(stored1PM);
 
-else if (this.id === "button-3PM") {
-  var  threePM = input3PM.val();
-  localStorage.setItem('stored3PM', threePM) 
-}
+  var stored2PM = localStorage.getItem("stored2PM");
+  $("#input-2PM").val(stored2PM);
 
-else if (this.id === "button-4PM") {
-  var  fourPM = input4PM.val();
-  localStorage.setItem('stored4PM', fourPM) 
-}
+  var stored3PM = localStorage.getItem("stored3PM");
+  $("#input-3PM").val(stored3PM);
 
-else if (this.id === "button-5PM") {
-  var  fivePM = input5PM.val();
-  localStorage.setItem('stored5PM', fivePM) 
-}
+  var stored4PM = localStorage.getItem("stored4PM");
+  $("#input-4PM").val(stored4PM);
 
+  var stored5PM = localStorage.getItem("stored5PM");
+  $("#input-5PM").val(stored5PM);
 
-}
-// created functions to retrieve stored input in each timeblock
-var   stored9AM = localStorage.getItem('stored9AM')
-$("#input-9AM").val(stored9AM)
-
-var   stored10AM = localStorage.getItem('stored10AM')
-$("#input-10AM").val(stored10AM)
-
-var   stored11AM = localStorage.getItem('stored11AM')
-$("#input-11AM").val(stored11AM)
-
-var   stored12PM = localStorage.getItem('stored12PM')
-$("#input-12PM").val(stored12PM)
-
-var   stored1PM = localStorage.getItem('stored1PM')
-$("#input-1PM").val(stored1PM)
-
-var   stored2PM = localStorage.getItem('stored2PM')
-$("#input-2PM").val(stored2PM)
-
-var   stored3PM = localStorage.getItem('stored3PM')
-$("#input-3PM").val(stored3PM)
-
-var   stored4PM = localStorage.getItem('stored4PM')
-$("#input-4PM").val(stored4PM)
-
-var   stored5PM = localStorage.getItem('stored5PM')
-$("#input-5PM").val(stored5PM)
-
- 
-
-  
-  
-
-
-
-  
-
-  
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
-  // created event listener to trigger local storage function upon clock of 
+  // created event listener to trigger local storage function upon click of
   // buttons on each timeblock
-  saveItem.on("click", saveText9AM)
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  saveItem.on("click", saveText9AM);
 });
-
-
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Work-Day-Scheduler
 
-#Description of Application:
-
+# Description Of Application:
 This site is a workday scheduler, that allows the user to input text into a timeblock for each hour of the workday. The text can be saved by clicking a "lock" button at the end of each timeblock, and the text inputted will persist upon reloading of the page due to local storage being utilized. As the day progresses, past hours will be grey in color, the current hour will be red, and future hours will be green.
 
-#Screenshot of Application:
+# Screenshot Of Application:
+<img src="./assets/images/work-day-scheduler" alt="screenshot">
 
+# Link To Deployed Website:
+https://999888z.github.io/Work-Day-Scheduler/
